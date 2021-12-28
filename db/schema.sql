abb2a385e3a088534f219d972f3cf9165463e6e5
KrisSmith7 <krissmithcodes@gmail.com>
2021-12-27 22:40:23 -0500

    joins dept data to roles table

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,8 @@
 DROP DATABASE IF EXISTS business_db;
 CREATE DATABASE business_db;
-
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS department;
 USE business_db;
 
 CREATE TABLE department(
@@ -13,7 +15,7 @@ CREATE TABLE roles(
     title VARCHAR(30),
     salary DECIMAL,
     department_id INTEGER,
-    FOREIGN KEY(department_id) REFERENCES department(id)
+    FOREIGN KEY(department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee(
@@ -22,10 +24,13 @@ CREATE TABLE employee(
     last_name VARCHAR(30),
     roles_id INT,
     manager_id INT,
-    FOREIGN KEY(roles_id) REFERENCES roles(id)
+    FOREIGN KEY(roles_id) REFERENCES roles(id) ON DELETE SET NULL
 );
 
 
+SELECT roles.*, department.dept_name AS department_name
+FROM roles
+LEFT JOIN department ON roles.department_id = department.id;
 
 
 -- GIVEN a command-line application that accepts user input
--- db/seeds.sql
+++ db/seeds.sql
@@ -7,12 +7,13 @@ VALUES ("Sales"),
 ("Legal"),
 ("Service");
 
--- INSERT INTO roles (title, salary, department_id)
--- VALUES ("Sales Lead", 80000, 1),
--- ("Lead Engineer", 150000, 2),
--- ("Lead Accountant", 100000, 3),
--- ("Lawyer", 175000, 4),
--- ("Customer Service Lead", 60000, 5);
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Lead", 80000, 1),
+("Lead Engineer", 150000, 2),
+("Lead Accountant", 100000, 3),
+("Lawyer", 175000, 4),
+("Customer Service Lead", 60000, 5),
+("Junior Accountant", 75000, 3);
 
 -- INSERT INTO employee (first_name, last_name, roles_id, manager_id)
 -- VALUES ("Jim", "Halpert", 1, 1),
