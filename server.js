2196158e21af01d05fc055310bce905d20886d3a
KrisSmith7 <krissmithcodes@gmail.com>
2022-01-01 21:08:15 -0500

    added functions to respond to inquirer

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const inputCheck = require('./utils/inputCheck');
+const { choices } = require('yargs');
 // const { viewAllEmployees } = require('./routes/apiRoutes/employeeRoutes');
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -46,9 +47,9 @@ function options() {
                     'Add an employee',
                     'Add a department',
                     'Add a role',
-//                     'Update employee role',
-//                     'Delete an employee',
-//                     'EXIT'
+                    'Update employee role',
+                    'Delete an employee',
+                    'EXIT'
                     ]
             }).then(function (answer) {
                 switch (answer.action) {
@@ -61,28 +62,28 @@ function options() {
                     case 'View all roles':
                         viewRoles();
                         break;
-//                     case 'Add an employee':
-//                         addEmployee();
-//                         break;
-//                     case 'Add a department':
-//                         addDepartment();
-//                         break;
-//                     case 'Add a role':
-//                         addRole();
-//                         break;
-//                     case 'Update employee role':
-//                         updateRole();
-//                         break;
-//                     case 'Delete an employee':
-//                         deleteEmployee();
-//                         break;
-//                     case 'EXIT': 
-//                         exitApp();
-//                         break;
-//                     default:
-//                         break;
-                }
-        })
+                    case 'Add an employee':
+                        addEmployee();
+                        break;
+                    case 'Add a department':
+                        addDepartment();
+                        break;
+                    case 'Add a role':
+                        addRole();
+                        break;
+                    case 'Update employee role':
+                        updateRole();
+                        break;
+                    case 'Delete an employee':
+                        deleteEmployee();
+                        break;
+                    case 'EXIT': 
+                        exitApp();
+                        break;
+                    default:
+                        break;
+        };
+    });
 };
 
 // view all employees in the database
@@ -119,5 +120,145 @@ function viewRoles() {
         options();
     })
 };
+
+// Create an employee
+function addEmployee() {
+    inquirer
+    .prompt([{
+        name:'first_name',
+        type:'input',
+        message:'first name?'},
+
+        {name:'last_name',
+        type:'input',
+        message:'last name?'},
+
+        {name:'roles_id',
+        type:'input',
+        message:'roles id?'},
+
+        {name:'manager_id',
+        type:'input',
+        message:'Manager id?'},
+    ])
+    .then (function (body) {
+    const sql = `INSERT INTO employee (first_name, last_name, roles_id, manager_id) 
+                  VALUES (?,?,?,?)`;
+    const params = [body.first_name, body.last_name, body.roles_id, body.manager_id];
+
+    db.query(sql, params, (err) => {
+        if (err) throw err;
+        console.log('Employee added.')
+        viewAllEmployees();
+    });
+});
+};
+
+// Create a department
+function addDepartment() {
+    inquirer
+    .prompt([{
+        name:'dept_name',
+        type:'input',
+        message:'dept name?'},
+    ])
+    .then (function (body) {
+    const sql = `INSERT INTO department (dept_name) 
+                  VALUES (?)`;
+    db.query(sql, body.dept_name, (err) => {
+        if (err) throw err;
+        console.log('Department added.')
+        viewDepartments();
+    });
+});
+};
+
+//Create a role
+function addRole() {
+    inquirer
+    .prompt([
+        {
+                name:'title',
+                type:'input',
+                message:"what's the name of this role?"
+        },
+        {
+                name:'salary',
+                type:'input',
+                message:"What's the salary for this role?"
+        },
+        {
+                name:'department_id',
+                type:'input',
+                message:"What's the dept id?"
+        }
+    ])
+    .then(function (body) {
+        const sql = `INSERT INTO roles (title, salary, department_id) 
+                          VALUES (?,?,?)`;
+            const params = [body.title, body.salary, body.department_id];
+            
+            db.query(sql, params, (err, result) =>  {
+                if (err) throw err;
+                console.log('Role added.')
+                viewRoles();
+            });
+});
+};
+
+//Update employee role
+function updateRole() {
+    inquirer
+    .prompt(
+        {
+            name: "employee_id",
+            type: "input",
+            message:"Which employee are you updating? Enter their employee id."
+        },
+        {
+        name:'roles_id',
+        type:'list',
+        message:'roles id?',
+        choices:
+            viewRoles()
+        },
+    )
+    .then(function (body) {
+
+        const sql = `UPDATE employee SET roles_id = ? WHERE id = ?`;
+        const params = [body.roles_id, body.employee_id];
+        
+        db.query(sql, params, (err, result) => {
+            if (err) throw err;
+            console.log("Employee updated.");
+            viewAllEmployees();
+    });
+});
+};
+
+function deleteEmployee() {
+    inquirer
+    .prompt(
+        {
+            name: "employee_id",
+            type: "input",
+            message:"Which employee are you deleting? Enter their employee id."
+        })
+        .then(function (body) {
+
+        const sql = `DELETE FROM employee WHERE id = ?`;
+        const param = [body.employee_id];
+        db.query(sql, param, (err) => {
+            if (err) throw err;
+            console.log ("Employee deleted.");
+            viewAllEmployees();
+            });
+
+    });
+};
+
+function exitApp () {
+    db.end();
+}
 
 options();
